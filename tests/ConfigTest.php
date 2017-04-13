2f139fd78b760b1f50d9a6f7e045242c936a1f2d
Timothy J Warren <tim@timshomepage.net>
2017-04-13 14:30:49 -0400

    Update version

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -10,7 +10,7 @@
  * @author      Timothy J. Warren <tim@timshomepage.net>
  * @copyright   2015 - 2017 Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     2.0.0
+ * @version     2.1.0
  * @link        https://git.timshomepage.net/timw4mail/ion
  */
 
