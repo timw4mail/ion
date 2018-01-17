128eeea639d5a67d053341a60c90b8ac9f689161
Timothy J Warren <tim@timshomepage.net>
2018-01-17 16:35:01 -0500

    Update the version in the header comments

--- tests/mocks.php
+++ tests/mocks.php
@@ -4,13 +4,13 @@
  *
  * Building blocks for web development
  *
- * PHP version 7
+ * PHP version 7.1
  *
  * @package     Ion
  * @author      Timothy J. Warren <tim@timshomepage.net>
- * @copyright   2015 - 2017 Timothy J. Warren
+ * @copyright   2015 - 2018 Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     2.2.0
+ * @version     2.3.0
  * @link        https://git.timshomepage.net/timw4mail/ion
  */
 
