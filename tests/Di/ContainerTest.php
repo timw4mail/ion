10682b8d606c8c9fa81733cfc8fb7d386bdffa36
Timothy J Warren <tim@timshomepage.net>
2019-12-05 15:39:02 -0500

    Update headers

--- tests/Di/ContainerTest.php
+++ tests/Di/ContainerTest.php
@@ -4,14 +4,14 @@
  *
  * Building blocks for web development
  *
- * PHP version 7.1
+ * PHP version 7.2
  *
  * @package     Ion
  * @author      Timothy J. Warren <tim@timshomepage.net>
- * @copyright   2015 - 2018 Timothy J. Warren
+ * @copyright   2015 - 2019 Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     2.4.1
- * @link        https://git.timshomepage.net/timw4mail/ion
+ * @version     3.0.0
+ * @link        https://git.timshomepage.net/aviat/ion
  */
 
 namespace Aviat\Ion\Tests\Di;
--- tests/XMLTest.php
+++ tests/XMLTest.php
@@ -1,17 +1,17 @@
 <?php declare(strict_types=1);
 /**
- * Hummingbird Anime List Client
+ * Ion
  *
- * An API client for Kitsu to manage anime and manga watch lists
+ * Building blocks for web development
  *
- * PHP version 7.1
+ * PHP version 7.2
  *
- * @package     HummingbirdAnimeClient
+ * @package     Ion
  * @author      Timothy J. Warren <tim@timshomepage.net>
- * @copyright   2015 - 2018  Timothy J. Warren
+ * @copyright   2015 - 2019 Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     4.1
- * @link        https://git.timshomepage.net/timw4mail/HummingBirdAnimeClient
+ * @version     3.0.0
+ * @link        https://git.timshomepage.net/aviat/ion
  */
 
 namespace Aviat\Ion\Tests;
